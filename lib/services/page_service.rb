688434a2526a8441d971c5643adab94343abcbfe
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-08-01 10:48:46 -0500

    Finished implementing the initial PageService

--- lib/services/page_service.rb
+++ lib/services/page_service.rb
@@ -5,7 +5,7 @@ require_relative '../factories/page_factory'
 module Services
   ##
   # This class contains all operations related to pages on a Jekyll website
-  class PageService
+  class PageService < BaseEditingService
     def initialize(repo_name, access_token)
         @github_service = GithubService.new(repo_name, access_token)
         @page_factory = PageFactory.new
@@ -29,8 +29,27 @@ module Services
       @page_factory.create_page(text_contents, nil)
     end
 
-    def save_page_update(file_path, file_contents, ref = nil, pr_body = '', reviewers = [])
+    def save_page_update(file_path, page_title, file_contents, ref = nil, pr_body = '', reviewers = [])
       if ref
+        ref_name = @github_service.get_ref_name_by_sha(ref)
+        sha_base_tree = @github_service.get_base_tree_for_branch(ref)
+    
+        new_tree_sha = create_new_tree(file_contents, page_title, file_path, sha_base_tree)
+        @github_service.commit_and_push_to_repo("Edited page #{page_title}", new_tree_sha, ref, ref_name)
+      else
+        branch_name = "editPage#{post_title.gsub(/\s+/, '')}"
+        ref_name = "heads/#{branch_name}"
+
+        master_head_sha = @github_service.get_master_head_sha
+        sha_base_tree = @github_service.get_base_tree_for_branch(master_head_sha)
+
+        @github_service.create_ref_if_necessary(ref_name, master_head_sha)
+        new_tree_sha = create_new_tree(file_contents, page_title, file_path, sha_base_tree)
+    
+        @github_service.commit_and_push_to_repo("Edited page #{page_title}", new_tree_sha, master_head_sha, ref_name)
+        @github_service.create_pull_request(branch_name, 'master', "Edited page #{post_title}",
+                                            pr_body,
+                                            reviewers)
       end
     end
   end
