e7d3d6f8e0ab9073c5cc56b75200281bf8df673a
andy-wojciechowski <wojciechowskia@msoe.edu>
2020-08-01 12:55:18 -0500

    Updating gem imports

--- lib/jekyll_github_pages.rb
+++ lib/jekyll_github_pages.rb
@@ -5,14 +5,15 @@ require 'carrierwave'
 
 # Source Files
 require 'factories/post_factory'
-require 'services/post_services/base_post_service'
-require 'services/post_services/post_creation_service'
-require 'services/post_services/post_editing_service'
-require 'services/post_services/post_pull_request_editing_service'
+require 'factories/page_factory'
+
+require 'services/base_editing_service'
 require 'services/github_service'
-
 require 'services/kramdown_service'
+require 'services/page_service'
+require 'services/post_service'
 
+require 'models/page'
 require 'models/post'
 require 'models/post_image_manager'
 
